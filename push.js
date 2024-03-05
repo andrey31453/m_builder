5bf2e5965b37a10144b5310d1a390620fffd8ab5
andrey31453 <andrey31453@google.com>
2024-03-05 09:40:05 +0300

    ggg

--- push.js
+++ push.js
@@ -1,10 +1,10 @@
 const { execSync } = require('child_process')
 const message = [...process.argv].slice(2).join(' ')
 
-execSync(`git submodule foreach 'git add *'`)
+execSync(`git submodule foreach 'git add .'`)
 execSync(`git submodule foreach 'git commit -m "${message}"'`)
 execSync(`git submodule foreach 'git push'`)
 
-execSync(`git add *`)
+execSync(`git add .`)
 execSync(`git commit -m "${message}"`)
 execSync(`git push`)
